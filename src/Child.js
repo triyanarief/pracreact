a6d2637a67f9c636c7b9715ca17f0fba15dbd13d
triyanarief <triyanarief@icloud.com>
2017-10-04 12:29:25 +0700

    sec commit

---
+++ src/Child.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Child extends React.Component {
+  render() {
+    return <div> {this.props.text} </div>
+  }
+}
+
+export default Child;
--- src/Mine.js
+++ src/Mine.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import Child from './Child';
 
 
 class Mine extends React.Component {
+  constructor(){
+    super()
+    this.state = {texts:[]}
+  }
+
   render() {
+    this.setState({texts: ['contoh pakai state pertama', 'contoh pakai state kedua', 'dan contoh pakai state ketiga']})
+
+    let texts = this.state.texts.map(function(text){
+      return(
+        <Child text = {text} />
+      )
+    })
     return ( // 1. kalau mau dibuat lebih dari satu baris maka harus dibuat pakai dalam kurung. jika cuma sebaris gak perlu contoh dibawah lebih dari sebaris
       // 2. render return() hanya boleh ada satu tag tunggal contoh div hanya boleh satu saja gak boleh ada dua. cara akalinya pakai div induk yang berisi tag div lain didalamnya. contoh dibawah
+      // 3. cara memasukkan child component bisa dilihat pada tag Child dibawah dan jangan lupa untuk export dari file asalnya dan kita import diatas agar bisa digunakan.
+      // 4. props yang dimaksud adalah kita bisa membuat props pada child dan nanti kita isi pada parrentnya disini tepatnya sebagai properti dalam tag child contoh dibawah
+      // 5. state digunakan di parrent agar bisa kita isi nilai dan kita rubah dengan menggunkan construct
       <div>
         <div id="mine">
           Halo Dunia Malam
@@ -12,6 +28,8 @@ class Mine extends React.Component {
         <div>
           Halo Dunia Siang
         </div>
+        <br />
+        {texts}
       </div>
     );
   }
